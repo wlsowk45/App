f5d35501bdae230cbf655a3189b052f94be57af5
sangmin <sangmin95@gmail.com>
2018-08-11 20:20:09 +0900

    second

---
+++ app/src/main/java/com/example/hyeokjin/knumeal/Restaurant.java
@@ -0,0 +1,13 @@
+package com.example.hyeokjin.knumeal;
+
+import android.app.Application;
+
+public class Restaurant extends Application {
+
+    public int min_person;
+    public String[] name;
+    public double longitude;
+    public double altitude;
+    public double latitude;
+
+}
