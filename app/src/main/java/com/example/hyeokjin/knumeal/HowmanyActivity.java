a1073889ddbc45492404e84777dfc0fa48912906
sangmin <sangmin95@gmail.com>
2018-08-12 17:02:52 +0900

    생성자까지 완

--- app/src/main/java/com/example/hyeokjin/knumeal/HowmanyActivity.java
+++ app/src/main/java/com/example/hyeokjin/knumeal/HowmanyActivity.java
@@ -25,36 +25,26 @@ public class HowmanyActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                str=editText.getText().toString();
-                num=Integer.parseInt(editText.getText().toString());
-                if(str.length()==0){
+                str = editText.getText().toString();
+                num = Integer.parseInt(editText.getText().toString());
+                if (str.length() == 0) {
                     textview.setText("값을 입력하시오");
-                }
-                else{
-                    if(num==1){
+                } else {
+                    if (num == 1) {
                         textview.setText("혼밥");
-                    }
-                    else if(num==2||num==3) {
+                    } else if (num == 2 || num == 3) {
                         textview.setText("2~3명 액티비티");
-                    }
-                    else if(num==4||num==5||num==6) {
+                    } else if (num == 4 || num == 5 || num == 6) {
                         textview.setText("4~6명 액티비티");
-                    }
-                    else if(num>=6) {
+                    } else if (num >= 6) {
                         textview.setText("6명이상 액티비티");
-                    }
-                    else{
+                    } else {
                         textview.setText("양의 정수를 입력하시오");
                     }
-
-
+                }
+            }
 
         });
-
-
-
-
-
 
     }
 }
--- app/src/main/java/com/example/hyeokjin/knumeal/Restaurant.java
+++ app/src/main/java/com/example/hyeokjin/knumeal/Restaurant.java
@@ -3,15 +3,46 @@ package com.example.hyeokjin.knumeal;
 import android.app.Application;
 
 public class Restaurant extends Application {
-    public int min_person;
-    public String[] name;
-    public double longitude;
-    public double altitude;
-    public double latitude;
-    public int test;
+
+    private int min_person;
+    private String name;
+    private double longitude;
+    private double latitude;
+
+
+    public Restaurant(String name, double longitude, double latitude, int min_person)
+    {
+        this.name = name;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.min_person = min_person;
+    }
 
 
 
+    public double getLongitude() {
+        return longitude;
+    }
 
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public int getMin_person() {
+
+        return min_person;
+    }
+
+    public void setMin_person(int min_person) {
+        this.min_person = min_person;
+    }
 
 }
