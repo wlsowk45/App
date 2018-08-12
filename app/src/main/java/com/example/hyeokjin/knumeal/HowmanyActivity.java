1dec3b13836b48fd734d64dddaf79e3df512a929
Hyeokjin <wlsowk45@gmail.com>
2018-08-12 20:39:27 +0900

    number

--- app/src/main/java/com/example/hyeokjin/knumeal/HowmanyActivity.java
+++ app/src/main/java/com/example/hyeokjin/knumeal/HowmanyActivity.java
@@ -18,8 +18,7 @@ public class HowmanyActivity extends AppCompatActivity {
 
     ArrayList<Restaurant> restaurant_main = new ArrayList<Restaurant>();
     ArrayList<Restaurant> restaurant_hall = new ArrayList<Restaurant>();
-
-
+    ArrayList<Restaurant> find_restaurant = new ArrayList<Restaurant>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +31,17 @@ public class HowmanyActivity extends AppCompatActivity {
 
         restaurant_main.add(new Restaurant("밥버거", 1, 2, 1));
         restaurant_hall.add(new Restaurant("밥버거1", 2, 3, 1));
-        restaurant_hall.add(new Restaurant("한솥", 3, 4, 2));
-        restaurant_hall.add(new Restaurant("한솥", 3, 4, 5));
-        restaurant_hall.add(new Restaurant("한솥", 3, 4, 3));
-        restaurant_hall.add(new Restaurant("한솥", 3, 4, 4));
-        restaurant_hall.add(new Restaurant("한솥", 3, 4, 5));
+        restaurant_hall.add(new Restaurant("한솥2", 3, 4, 2));
+        restaurant_hall.add(new Restaurant("한솥3", 3, 4, 2));
+        restaurant_hall.add(new Restaurant("한솥4", 3, 4, 3));
+        restaurant_hall.add(new Restaurant("한솥5", 3, 4, 4));
+        restaurant_hall.add(new Restaurant("한솥6", 3, 4, 5));
 
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 str = editText.getText().toString();
                 num = Integer.parseInt(editText.getText().toString());
                 if (str.length() == 0) {
@@ -49,21 +49,46 @@ public class HowmanyActivity extends AppCompatActivity {
                 } else {
                     if (num == 1) {
                         textview.setText("혼밥");
+                        for(int i=0;i<restaurant_hall.size();i++)
+                        {
+                            if(restaurant_hall.get(i).getMin_person()<2)
+                                find_restaurant.add(restaurant_hall.get(i));
+                        }
+
                     } else if (num == 2 || num == 3) {
                         textview.setText("2~3명 액티비티");
+
+                        for(int i=0;i<restaurant_hall.size();i++)
+                        {
+                            if(restaurant_hall.get(i).getMin_person()<4)
+                                find_restaurant.add(restaurant_hall.get(i));
+                        }
                     } else if (num == 4 || num == 5 || num == 6) {
                         textview.setText("4~6명 액티비티");
+
+                        for(int i=0;i<restaurant_hall.size();i++)
+                        {
+                            if(restaurant_hall.get(i).getMin_person()<7)
+                                find_restaurant.add(restaurant_hall.get(i));
+                        }
+
                     } else if (num >= 6) {
                         textview.setText("6명이상 액티비티");
+
+                        for(int i=0;i<restaurant_hall.size();i++)
+                        {
+                            if(restaurant_hall.get(i).getMin_person()>=7)
+                                find_restaurant.add(restaurant_hall.get(i));
+                        }
+
                     } else {
                         textview.setText("양의 정수를 입력하시오");
                     }
                 }
 
-                Intent i = new Intent(getApplicationContext(), onepeopleActivity.class);
-                i.putParcelableArrayListExtra("main_gate", restaurant_hall);
-                startActivity(i);
-
+                Intent intent = new Intent(getApplicationContext(), onepeopleActivity.class);
+                intent.putParcelableArrayListExtra("main_gate", find_restaurant);
+                startActivity(intent);
                 //test
 
             }
