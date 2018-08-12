f59009a97d93acc2b6e6a009c77ae6c2acd90740
Hyeokjin <wlsowk45@gmail.com>
2018-08-12 20:17:21 +0900

    number

---
+++ app/src/main/java/com/example/hyeokjin/knumeal/onepeopleActivity.java
@@ -0,0 +1,27 @@
+package com.example.hyeokjin.knumeal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class onepeopleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onepeople);
+
+        ArrayList<Restaurant> restaurants_main;
+        Intent i = getIntent();
+        restaurants_main = i.getParcelableArrayListExtra("main_gate");
+        TextView textView1=(TextView)findViewById(R.id.textView);
+        TextView textView2=(TextView)findViewById(R.id.textView2);
+
+        textView1.setText(restaurants_main.get(0).getName());
+        textView2.setText(restaurants_main.get(1).getName());
+
+    }
+}
