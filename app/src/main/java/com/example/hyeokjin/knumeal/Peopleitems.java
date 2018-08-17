4ac074e5e10790c2adbabf3646886c85f6f5fd0a
Hyeokjin <wlsowk45@gmail.com>
2018-08-17 16:25:22 +0900

    Merge branch 'master' of https://github.com/wlsowk45/App into Hyeokjin3
    # Conflicts:
    #	app/src/main/java/com/example/hyeokjin/knumeal/HowmanyActivity.java
    #	app/src/main/java/com/example/hyeokjin/knumeal/MainActivity.java

--- app/src/main/java/com/example/hyeokjin/knumeal/Peopleitems.java
+++ app/src/main/java/com/example/hyeokjin/knumeal/Peopleitems.java
@@ -1,12 +1,29 @@
 package com.example.hyeokjin.knumeal;
 
+import android.widget.CheckBox;
+
 public class Peopleitems {
 
     String name;
+    boolean checking;
+    CheckBox checkBox;
+
+    public Peopleitems(boolean checking) {
+        this.checking = checking;
+    }
+
+    public boolean isChecking(CheckBox checkBox){
+        if(checkBox.isChecked())
+            return true;
+        else
+            return  false;
+
+    }
 
     public Peopleitems(String name) {
         this.name = name;
     }
+
 
     public String getName() {
         return name;
