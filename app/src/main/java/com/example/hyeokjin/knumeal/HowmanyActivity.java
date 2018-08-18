7ea7e9c24651f185d19a92d06a224ea41a336ef3
sangmin <sangmin95@gmail.com>
2018-08-18 18:15:33 +0900

    No error

--- app/src/main/java/com/example/hyeokjin/knumeal/HowmanyActivity.java
+++ app/src/main/java/com/example/hyeokjin/knumeal/HowmanyActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -37,54 +38,56 @@ public class HowmanyActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 str = editText.getText().toString();
-                num = Integer.parseInt(editText.getText().toString());
-                if (str.length() == 0) {
-                    textview.setText("값을 입력하시오");
-                } else {
+
+                if(str.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(),"인원수를 입력하세요",Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+                    num = Integer.parseInt(editText.getText().toString());
+
+
                     if (num == 1) {
                         textview.setText("혼밥");
-                        for(int i=0;i<restaurants.size();i++)
-                        {
-                            if(restaurants.get(i).getMin_person()<2)
+                        for (int i = 0; i < restaurants.size(); i++) {
+                            if (restaurants.get(i).getMin_person() < 2)
                                 find_restaurant.add(restaurants.get(i));
                         }
 
                     } else if (num == 2 || num == 3) {
                         textview.setText("2~3명 액티비티");
 
-                        for(int i=0;i<restaurants.size();i++)
-                        {
-                            if(restaurants.get(i).getMin_person()>=2 && restaurants.get(i).getMin_person()<4)
+                        for (int i = 0; i < restaurants.size(); i++) {
+                            if (restaurants.get(i).getMin_person() >= 2 && restaurants.get(i).getMin_person() < 4)
                                 find_restaurant.add(restaurants.get(i));
                         }
                     } else if (num == 4 || num == 5 || num == 6) {
                         textview.setText("4~6명 액티비티");
 
-                        for(int i=0;i<restaurants.size();i++)
-                        {
-                            if(restaurants.get(i).getMin_person()>=4&&restaurants.get(i).getMin_person()<7)
+                        for (int i = 0; i < restaurants.size(); i++) {
+                            if (restaurants.get(i).getMin_person() >= 4 && restaurants.get(i).getMin_person() < 7)
                                 find_restaurant.add(restaurants.get(i));
                         }
                     } else if (num >= 6) {
                         textview.setText("6명이상 액티비티");
 
-                        for(int i=0;i<restaurants.size();i++)
-                        {
-                            if(restaurants.get(i).getMin_person()>=7)
+                        for (int i = 0; i < restaurants.size(); i++) {
+                            if (restaurants.get(i).getMin_person() >= 7)
                                 find_restaurant.add(restaurants.get(i));
                         }
                     } else {
                         textview.setText("양의 정수를 입력하시오");
                     }
+
+
+                    //HowMany -> onepeople
+                    Intent intent = new Intent(HowmanyActivity.this, onepeopleActivity.class);
+                    intent.putParcelableArrayListExtra("To one", find_restaurant);
+                    startActivity(intent);
+                    find_restaurant.clear();
+
                 }
-
-                //HowMany -> onepeople
-                Intent intent=new Intent(HowmanyActivity.this,onepeopleActivity.class);
-                intent.putParcelableArrayListExtra("To one", find_restaurant);
-                startActivity(intent);
-                find_restaurant.clear();
-
-
 
                 //test
 
