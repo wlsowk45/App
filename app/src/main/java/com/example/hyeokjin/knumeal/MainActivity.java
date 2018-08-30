5da2ddb1b08a178f5f3843edf058e91a160f00b9
Hyeokjin <wlsowk45@gmail.com>
2018-08-30 16:17:45 +0900

    Merge branch 'master' of https://github.com/wlsowk45/App into Hyeokjin3
    # Conflicts:
    #	app/src/main/java/com/example/hyeokjin/knumeal/HowmanyActivity.java
    #	app/src/main/java/com/example/hyeokjin/knumeal/MainActivity.java

--- app/src/main/java/com/example/hyeokjin/knumeal/MainActivity.java
+++ app/src/main/java/com/example/hyeokjin/knumeal/MainActivity.java
@@ -24,14 +24,41 @@ public class MainActivity extends AppCompatActivity {
         final ArrayList<Restaurant> restaurants = new ArrayList<Restaurant>();
 
         restaurants.add(new Restaurant("밥버거", 1, 2, 1,"Hall_gate",3000));
-        restaurants.add(new Restaurant("밥버거1", 2, 3, 1,"Main_gate",3000));
-        restaurants.add(new Restaurant("밥버거2", 2, 3, 1,"Main_gate",3000));
-        restaurants.add(new Restaurant("한솥2", 3, 4, 2,"Main_gate",3000));
-        restaurants.add(new Restaurant("한솥3", 3, 4, 2,"Main_gate",4000));
-        restaurants.add(new Restaurant("한솥4", 3, 4, 3,"Main_gate",5000));
-        restaurants.add(new Restaurant("한솥5", 3, 4, 4,"Main_gate",6000));
-        restaurants.add(new Restaurant("한솥6", 3, 4, 5,"Main_gate",7000));
-        restaurants.add(new Restaurant("맘스터치",35.885838,128.610095,1,"Hall_gate",5000));
+        restaurants.add(new Restaurant("종이밥", 2, 3, 1,"Hall_gate",3000));
+        restaurants.add(new Restaurant("우리분식", 3, 4, 2,"Hall_gate",5000));
+        restaurants.add(new Restaurant("동아리분식", 3, 4, 2,"Hall_gate",5000));
+        restaurants.add(new Restaurant("맛나분식", 35.885397, 128.609850, 2,"Hall_gate",5000));
+        restaurants.add(new Restaurant("죠스찜닭", 35.885287, 128.609281, 3,"Hall_gate",7000));
+        restaurants.add(new Restaurant("순대국밥", 3, 4, 2,"Hall_gate",6000));
+        restaurants.add(new Restaurant("맘스터치",35.885838,128.610095,1,"Hall_gate",5500));
+        restaurants.add(new Restaurant("GS편의점", 3, 4, 1,"Hall_gate",1100));
+        restaurants.add(new Restaurant("다이마루", 3, 4, 2,"Hall_gate",8000));
+        restaurants.add(new Restaurant("돈까스백작", 3, 4, 2,"Hall_gate",6000));
+        restaurants.add(new Restaurant("피자빙고", 35.883999, 128.609, 2,"Hall_gate",7000));
+        restaurants.add(new Restaurant("경대밥집", 35.884159, 128.609647, 2,"Hall_gate",6000));
+        restaurants.add(new Restaurant("정글로", 3, 4, 2,"Hall_gate",7000));
+        restaurants.add(new Restaurant("세계지도", 3, 4, 1,"Hall_gate",4000));
+        restaurants.add(new Restaurant("몬나니떡볶이", 3, 4, 2,"Hall_gate",4000));
+        restaurants.add(new Restaurant("북경반점", 3, 4, 2,"Hall_gate",4000));
+        restaurants.add(new Restaurant("해금강", 3, 4, 2,"Hall_gate",3000));
+        restaurants.add(new Restaurant("쪽문분식", 3, 4, 2,"Hall_gate",4000));
+        restaurants.add(new Restaurant("뼈찜", 3, 4, 1,"Hall_gate",6000));
+        restaurants.add(new Restaurant("한솥", 2, 3, 1,"Main_gate",3000));
+        restaurants.add(new Restaurant("모이다식탁", 2, 3, 1,"Main_gate",5500));
+        restaurants.add(new Restaurant("불멸의 떡볶이", 2, 3, 1,"Main_gate",4000));
+        restaurants.add(new Restaurant("청춘라면", 2, 3, 1,"Main_gate",3000));
+        restaurants.add(new Restaurant("김밥천국", 2, 3, 1,"Main_gate",3000));
+        restaurants.add(new Restaurant("왕얼큰이", 2, 3, 1,"Main_gate",4000));
+        restaurants.add(new Restaurant("경대루", 2, 3, 1,"Main_gate",4000));
+        restaurants.add(new Restaurant("경주국밥", 2, 3, 1,"Main_gate",5500));
+        restaurants.add(new Restaurant("땅땅치킨", 2, 3, 1,"Main_gate",9000));
+        restaurants.add(new Restaurant("미쳐버린 파닭", 2, 3, 1,"Main_gate",5500));
+
+
+
+
+
+
 
 
 
--- app/src/main/java/com/example/hyeokjin/knumeal/onepeopleActivity.java
+++ app/src/main/java/com/example/hyeokjin/knumeal/onepeopleActivity.java
@@ -20,6 +20,7 @@ public class onepeopleActivity extends AppCompatActivity {
     ArrayList<Restaurant> found_restaurant = new ArrayList<Restaurant>();
     ArrayList<Restaurant> checked_restaurant = new ArrayList<Restaurant>();
     PeopleAdapter adapter;
+    int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +32,37 @@ public class onepeopleActivity extends AppCompatActivity {
         //ArrayList<Restaurant> final_restaurant = new ArrayList<Restaurant>();
         Intent i = getIntent();
         found_restaurant = i.getParcelableArrayListExtra("To one");
-        ListView listView=(ListView)findViewById(R.id.listview);
+        final ListView listView=(ListView)findViewById(R.id.listview);
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+
         adapter=new PeopleAdapter();
         for(int j=0;j<found_restaurant.size();j++){
             adapter.addItem(new Peopleitems(found_restaurant.get(j).getName()));
         }
 
         listView.setAdapter(adapter);
+
+        final CheckBox allcheck=(CheckBox)findViewById(R.id.checkBox2);
+        count=adapter.getCount();
+
+        allcheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for(int j=0;j<count;j++){
+                    listView.setItemChecked(j,false);
+                }
+
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+
+
+
+
+
+
 
 
         Button button=(Button)findViewById(R.id.button);
@@ -65,7 +90,27 @@ public class onepeopleActivity extends AppCompatActivity {
     }
     class PeopleAdapter extends BaseAdapter {
 
+
+
         ArrayList<Peopleitems> items = new ArrayList<Peopleitems>();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         @Override
         public int getCount() {
@@ -92,16 +137,30 @@ public class onepeopleActivity extends AppCompatActivity {
         }
 
         @Override
-        public View getView(int i, View convertview, ViewGroup viewGroup) {
+        public View getView(final int i, View convertview, ViewGroup viewGroup) {
             peopleitemviewcod view = new peopleitemviewcod(getApplicationContext());
             final Peopleitems item = items.get(i);
             view.setName(item.getName());
-            CheckBox checkBox=(CheckBox)view.findViewById(R.id.checkBox);
+            final CheckBox checkBox=(CheckBox)view.findViewById(R.id.checkBox);
+            if(item.isChecked()){
+                checkBox.setChecked(true);
+
+            }
+            else{
+                checkBox.setChecked(false);
+
+            }
+
+
+
+
+
             checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                     if(b){
                         item.setChecked(true);
+
                         Toast.makeText(getApplicationContext(),"체크된 식당"+item.getName()+item.isChecked(),Toast.LENGTH_SHORT).show();
 
                     }
@@ -109,9 +168,11 @@ public class onepeopleActivity extends AppCompatActivity {
                         item.setChecked(false);
                         Toast.makeText(getApplicationContext(),"체크X"+item.getName()+item.isChecked(),Toast.LENGTH_SHORT).show();
 
+
                     }
                 }
             });
+
 
             return view;
 
